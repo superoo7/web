1c73d6ee11641f2036e28cd077bafa842f61c54f
Sebastian Kim <sydneyitguy@gmail.com>
2018-10-26 01:19:03 +0900

    Add localStorage polyfill

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -65,3 +65,13 @@ if (!Array.prototype.includes) {
     }
   });
 }
+
+if (!('localStorage' in window)) {
+  window.localStorage = {
+    _data       : {},
+    setItem     : function(id, val) { return this._data[id] = String(val); },
+    getItem     : function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
+    removeItem  : function(id) { return delete this._data[id]; },
+    clear       : function() { return this._data = {}; }
+  };
+}

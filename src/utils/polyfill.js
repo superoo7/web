71ee7c366c9526334ad23089875e8b11ebe571de
Sebastian Kim <sydneyitguy@gmail.com>
2018-10-29 01:45:30 +0900

    Array.find polyfill

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -68,6 +68,30 @@ if (!Array.prototype.includes) {
   });
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
 if (!('localStorage' in window)) {
   window.localStorage = {
     _data       : {},

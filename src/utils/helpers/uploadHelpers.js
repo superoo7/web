de4d1e30766e906af93feb59d2ffeac933db7524
Sebastian Kim <sydneyitguy@gmail.com>
2018-10-29 23:55:04 +0900

    Handle cancel image selection

--- src/utils/helpers/uploadHelpers.js
+++ src/utils/helpers/uploadHelpers.js
@@ -4,6 +4,10 @@ import { notification } from 'antd';
 const UPLOAD_API = `${process.env.REACT_APP_API_ROOT}/posts/upload`;
 
 export function validateImage(file) {
+  if (!file) {
+    return false;
+  }
+
   if (!file.type.match(/png|jpg|jpeg|gif/)) { // because `accept` doesn't work on some browsers
     notification['error']({ message: 'You can only upload standard image files (png, gif, jpg).' });
     return false;
